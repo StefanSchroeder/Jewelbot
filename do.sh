f3124374487cef67a0551dfd61920471a4e6e37a
Stefan Schroeder <ondekoza@gmail.com>
2017-03-22 07:20:07 +0100

    cleanup

--- do.sh
+++ do.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 JEWELS=8
-INC=/home/horst/perl5/lib/perl5/ 
+
 while [ 1 ] 
 do 
-        echo "##################### Starting Retrieval."
-	perl -I$INC imager.pl $JEWELS > problem.txt 2> w.err
-	cat problem.txt
-        echo "##################### Solving Problem"
-	perl -I$INC click_at_tile.pl --jewels $JEWELS `perl solve.pl problem.txt`
+	perl imager.pl $JEWELS > problem.txt 2> w.err
+	perl click_at_tile.pl --fixy=24 `perl solve.pl problem.txt`
 	sleep 1
-        echo "##################### DONE "
 done
 
